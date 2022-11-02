6784c97a361e18d5b21ac76572150d9290d6af23
Cody Glover <cody_glover@progressive.com>
2022-11-02 14:12:16 -0400

    next changes

--- gitdemo/gitdemo/Program.cs
+++ gitdemo/gitdemo/Program.cs
@@ -6,7 +6,13 @@ namespace gitdemo
     {
         static void Main(string[] args)
         {
+            GetName();
             Console.WriteLine("Hello World!");
+        }
+
+        private static void GetName()
+        {
+            Console.WriteLine("Some text");
         }
     }
 }
